c1a0422036584042c63e2ed498afbdea25597e45
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-07-31 14:28:33 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+
+// Importing Modules
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// importing files
+const routes = require('./routes');
+
+// Define Global Variables
+const app = express();
+const log = console.log;
+const PORT = process.env.PORT || 8080; // Step 1
+
+const MONGODB_URI = 'mongodb+srv://maiuran:maiuran@rest.m3zlv.mongodb.net/rest?retryWrites=true&w=majority';
+
+// Step 2
+mongoose.connect( MONGODB_URI || 'mongodb://localhost/my_database', {
+    useNewUrlParser: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Moongoooseee is connectedd boii!')
+})
+
+// Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes);
+
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
+
+app.listen(PORT, () => {
+    log(`Server is starting at PORT: ${PORT}`);
+});
